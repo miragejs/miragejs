d69680947c1992eabb4e6b6ca738137d9e681b3c
Sam Selikoff <sam.selikoff@gmail.com>
2015-01-30 16:34:05 -0500

    clean up old config

--- tests/dummy/app/pretender/config.js
+++ tests/dummy/app/pretender/config.js
@@ -1,72 +1,10 @@
 export default function() {
-  // var store = this.store;
-
-  // this.stub('get', 'products', ['products', 'categories']);
-
-  // this.stub('get', '/contacts', function() {
-  //   return ['contacts'];
-  // });
-  // this.stub('get', '/contacts', 'contacts');
+  // contacts
   this.stub('get', '/contacts');
   this.stub('post', '/contacts');
-  // this.stub('get', '/contacts/:id', 'contact');
 
-  // this.stub('get', '/contacts', function(store, request) {
-  //   var contacts = store.find('contacts');
-
-  //   return {
-  //     contacts: contacts
-  //   };
-  // });
-
-  // this.stub('get', '/contacts/:id', function(store, request) {//   var contact = store.find('contacts', request.params.id);
-  //   var contactId = +request.params.id;
-
-  //   var contact = store.find('contacts', contactId);
-  //   var addresses = store.find('addresses')
-  //     .filterBy('contact_id', +contactId);
-
-  //   return {
-  //     contact: contact,
-  //     addresses: addresses
-  //   };
-  // });
+  // contact
   this.stub('get', '/contacts/:id');
-  this.stub('put', '/contacts/:id', 'contact');
+  this.stub('put', '/contacts/:id');
   this.stub('delete', '/contacts/:id');
-  // this.stub('get', '/contacts/:id', ['contact', 'addresses']);
-
-  // this.stub('get', '/contacts/:id', function() {
-  //   return ['contacts'];
-  // });
-
-  // this.stub('get', 'products', {
-  //   products: store.find('product'),
-  //   cateogires: store.find('category')
-  // });
-
-  // this.stub('get', 'products', function(request) {
-
-  //   var response = {
-  //     data: {
-  //       products: server.data.products
-  //     },
-  //     code: 200
-  //   };
-
-  //   return response;
-  // });
-
-  // this.stub('get', 'users', function(request) {
-
-  //   var response = {
-  //     data: {
-  //       products: server.data.products
-  //     },
-  //     code: 200
-  //   };
-
-  //   return response;
-  // });
-
 }
