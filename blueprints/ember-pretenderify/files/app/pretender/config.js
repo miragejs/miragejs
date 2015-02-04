303f5d0a5a43e697d52d8b29f4d37633f85082a1
Sam Selikoff <sam.selikoff@gmail.com>
2015-02-04 10:47:31 -0500

    update blueprint, readme

--- blueprints/ember-pretenderify/files/app/pretender/config.js
+++ blueprints/ember-pretenderify/files/app/pretender/config.js
@@ -1,7 +1,9 @@
 export default function() {
 
+  // These comments are here to help you get started. Feel free to delete them.
+
   /*
-    Config, defaults below
+    Default config
   */
   // this.namespace = 'api';
   // this.timing = 400;
@@ -25,10 +27,49 @@ export default function() {
   */
 
   /*
+    POST shorthands
+
+    this.stub('post', '/contacts');
+    this.stub('post', '/contacts', 'user'); // specify the type of resource to be created
+  */
+
+  /*
     PUT shorthands
 
-    this.stub('get', '/contacts')
-    this.stub('get', '/contacts', 'users')
-    this.stub('get', '/contacts', ['contacts', 'addresses'])
+    this.stub('put', '/contacts/:id');
+    this.stub('put', '/contacts/:id', 'user'); // specify the type of resource to be updated
+  */
+
+  /*
+    DELETE shorthands
+
+    this.stub('delete', '/contacts/:id')
+    this.stub('delete', '/contacts/:id', 'user') // specify the type of resource to be deleted
+
+    // Single object + related resources. Make sure parent resource is first.
+    this.stub('delete', '/contacts/:id', ['contact', 'addresses']);
+  */
+
+  /*
+    Function fallback. Manipulate data in the store via
+
+      - store.find(key)
+      - store.find(key, id)-
+      - store.push(key, data)
+      - store.delete(key, id)
+
+    // Example: return a single object with related models
+    this.stub('get', '/contacts/:id', function(store, request) {
+      var contactId = +request.params.id;
+      var contact = store.find('contact', contactId);
+      var addresses = store.find('address')
+        .filterBy('contact_id', contactId);
+
+      return {
+        contact: contact,
+        addresses: addresses
+      };
+    });
+
   */
 }
