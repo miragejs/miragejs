4ba84ad2b85f858dbe59f5c8587b9bcbaf78f906
Sam Selikoff <sam.selikoff@gmail.com>
2015-02-04 09:57:27 -0500

    add blueprint

---
+++ blueprints/ember-pretenderify/files/app/pretender/config.js
@@ -0,0 +1,34 @@
+export default function() {
+
+  /*
+    Config, defaults below
+  */
+  // this.namespace = 'api';
+  // this.timing = 400;
+
+
+  /*
+    Routes
+  */
+  /*
+    GET shorthands
+
+    // Collections
+    this.stub('get', '/contacts')
+    this.stub('get', '/contacts', 'users')
+    this.stub('get', '/contacts', ['contacts', 'addresses'])
+
+    // Single objects
+    this.stub('get', '/contacts/:id')
+    this.stub('get', '/contacts/:id', 'user')
+    this.stub('get', '/contacts/:id', ['contact', 'addresses'])
+  */
+
+  /*
+    PUT shorthands
+
+    this.stub('get', '/contacts')
+    this.stub('get', '/contacts', 'users')
+    this.stub('get', '/contacts', ['contacts', 'addresses'])
+  */
+}
