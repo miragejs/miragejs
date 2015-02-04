0c5b5bf3237f9266ebc454eb500910132247c48e
Sam Selikoff <sam.selikoff@gmail.com>
2015-02-04 10:49:54 -0500

    add sample data file

---
+++ blueprints/ember-pretenderify/files/app/pretender/data/contacts.js
@@ -0,0 +1,16 @@
+/*
+  This is an example. This data will be added to the store
+  under the `contacts` key.
+
+  Create more files in this directory to add more data.
+*/
+export default [
+  {
+    id: 1,
+    name: 'Zelda'
+  },
+  {
+    id: 2,
+    name: 'Link'
+  },
+];
