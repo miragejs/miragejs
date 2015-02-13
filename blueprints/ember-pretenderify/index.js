f4826422abfac36f75fe99465d9c06accc4d8c16
Sam Selikoff <sam.selikoff@gmail.com>
2015-02-13 17:32:17 -0500

    unreached val after return #pwned

--- blueprints/ember-pretenderify/index.js
+++ blueprints/ember-pretenderify/index.js
@@ -8,7 +8,9 @@ module.exports = {
   },
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('pretender', '~0.6.0');
-    return this.addBowerPackageToProject('ember-inflector', '~1.3.1');
+    return this.addBowerPackagesToProject([
+      {name: 'pretender', target: '~0.6.0'},
+      {name: 'ember-inflector', target: '~1.3.1'}
+    ]);
   }
 };
