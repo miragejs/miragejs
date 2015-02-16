8fba187408dcdeb0b49eae805531c70da1cfdb9c
Sam Selikoff <sam.selikoff@gmail.com>
2015-02-16 12:57:31 -0500

    move stub to server, delete defaults

--- addon/server.js
+++ addon/server.js
@@ -1,15 +1,23 @@
 import Pretender from 'pretender';
-import defaults from './defaults';
 import loadData from './load-data';
-import store from 'ember-pretenderify/store';
+import store from './store';
+import frontController from './controllers/front';
 
 export default function(options) {
+  // Init vars
+  var environment = options.environment;
+  var userConfig = options.userConfig;
+  var modulePrefix = options.modulePrefix;
 
-  this.server = new Pretender(function() {
+  // Default properties
+  this.timing = 400;
+  this.namespace = '';
 
+  // Methods
+  this.store = store;
+
+  this.interceptor = new Pretender(function() {
     // Default Pretender config
-    this.timing = 400;
-
     this.prepareBody = function(body) {
       return body ? JSON.stringify(body) : '{"error": "not found"}';
     };
@@ -17,22 +25,38 @@ export default function(options) {
     this.unhandledRequest = function(verb, path) {
       console.error("Your Ember app tried to " + verb + " '" + path + "', but there was no Pretender route defined to handle this request.");
     };
-
-    defaults.call(this, options.environment);
-
-    // Add user config, which adds routes + overrides defaults
-    options.userConfig.call(this);
   });
 
-  this.server.store = store;
+  this.stub = function(verb, path, handler, code) {
+    var _this = this;
+    var interceptor = this.interceptor;
+    var timing = environment === 'test' ? 0 : this.timing;
+    path = path[0] === '/' ? path.slice(1) : path;
 
-  if (options.environment !== 'test') {
-    loadData(options.modulePrefix, this.server.store);
+    interceptor[verb].call(interceptor, this.namespace + '/' + path, function(request) {
+
+      var response = frontController.handle(verb, handler, _this.store, request, code);
+
+      if (environment !== 'test') {
+        console.log('Successful request: ' + verb.toUpperCase() + ' ' + request.url);
+        console.log(response[2]);
+      }
+
+      return response;
+    }, timing);
+  };
+
+  // Init
+  // Add user config, which adds routes + overrides defaults
+  userConfig.call(this);
+
+  if (environment !== 'test') {
+    loadData(modulePrefix, this.store);
   }
 
-  if (options.environment === 'test') {
-    window.store = this.server.store;
+  if (environment === 'test') {
+    window.store = this.store;
   }
 
-  return this.server;
+  return this;
 }
