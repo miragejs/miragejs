77b5c67507e91465895d41ae9fe9dcb04fe76ed6
Sam Selikoff <sam.selikoff@gmail.com>
2015-02-18 09:16:26 -0500

    extract factory to sep class

--- addon/factory.js
+++ addon/factory.js
@@ -1,5 +1,17 @@
 export default {
-  define: function() {
+  define: function(type, attrs) {
+    if (attrs === undefined) {
+      attrs = type;
+    }
 
+    return function(n) {
+      var newModel = {};
+
+      Object.keys(attrs).forEach(function(key) {
+        newModel[key] = attrs[key];
+      });
+
+      return newModel;
+    };
   }
 };
--- addon/index.js
+++ addon/index.js
@@ -1,21 +1,5 @@
+import Factory from './factory';
+
 export default {
-
-  Factory: {
-    define: function(type, attrs) {
-      if (attrs === undefined) {
-        attrs = type;
-      }
-
-      return function(n) {
-        var newModel = {};
-
-        Object.keys(attrs).forEach(function(key) {
-          newModel[key] = attrs[key];
-        });
-
-        return newModel;
-      };
-    }
-  }
-
+  Factory: Factory
 };
