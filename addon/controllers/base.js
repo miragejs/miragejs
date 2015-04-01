67a3ec2e588902b542783bdc0d3880155b4b1a31
Sam Selikoff <sam.selikoff@gmail.com>
2015-04-01 15:13:40 -0400

    no es6 in /addon :(

--- addon/controllers/base.js
+++ addon/controllers/base.js
@@ -5,10 +5,11 @@ export default Ember.Object.extend({
 
   functionHandler: function(handler, db, request) {
     var data;
+
     try {
       data = handler(db, request);
     } catch(error) {
-      console.error(`Mirage: Your custom function handler for the url ${request.url} threw an error:`, error.message, error.stack);
+      console.error('Mirage: Your custom function handler for the url ' + request.url + ' threw an error:', error.message, error.stack);
     }
 
     return data;
