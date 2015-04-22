b63e3cd55153d1c971ee7bf6197ca4fc13a8f274
Miguel Camba <miguel.camba@gmail.com>
2015-04-22 11:31:36 +0100

    Added also tests for `delete` verb

--- tests/unit/controller-test.js
+++ tests/unit/controller-test.js
@@ -75,9 +75,41 @@ test('its default response is 201 if the verb is post', function(assert) {
   assert.equal(response[0], 204);
 });
 
-test('its default response is 204 if the verb is delete', function(assert) {
+test('its default response is 204 if the verb is delete and the response is empty', function(assert) {
   var response = controller.handle('delete', {});
   assert.equal(response[0], 204);
+});
+
+test('its default response is 200 if the verb is delete and the response is not empty and no specific code passed', function(assert) {
+  var response = controller.handle('delete', function() {
+    return { text: 'thanks' };
+  });
+  assert.equal(response[0], 200, 'Returning a non-empty object changes the default code to 200');
+
+  var response2 = controller.handle('delete', function() {
+    return [];
+  });
+  assert.equal(response2[0], 200, 'An empty array IS NOT an empty response');
+
+  var response3 = controller.handle('delete', function() {
+    return;
+  });
+  assert.equal(response3[0], 204, 'undefined is considered an empty response');
+
+  var response4 = controller.handle('delete', function() {
+    return '';
+  });
+  assert.equal(response4[0], 204, 'An empty string is considered and empty response');
+
+  var response5 = controller.handle('delete', function() {
+    return;
+  }, 204);
+  assert.equal(response4[0], 204, 'If the response code is forced, that takes precedence');
+
+  var response6 = controller.handle('delete', function() {
+    return {};
+  }, 204);
+  assert.equal(response6[0], 204, 'An empty object is considered and empty response');
 });
 
 // TODO: Use spies to ensure get#shorthand is called with appropriate args
