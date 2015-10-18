HeroicEric/ember-cli-mirage

Remove reliance on lodash global

app/initializers/ember-cli-mirage.js
@@ -1,12 +1,13 @@
 import ENV from '../config/environment';
-import baseConfig, { testConfig } from '../mirage/config';
 import Server from 'ember-cli-mirage/server';
+import _keys from 'lodash/object/keys';
+import baseConfig, { testConfig } from '../mirage/config';
 import readModules from 'ember-cli-mirage/utils/read-modules';
 
 function hasModulesOfType(modulesMap, type) {
   var modulesOfType = modulesMap[type] || {};
 
-  return _.keys(modulesOfType).length > 0;
+  return _keys(modulesOfType).length > 0;
 }
 
 export default {

miragejs/miragejs

app/initializers/ember-cli-mirage.js
@@ -2,6 +2,7 @@ import readModules from 'ember-cli-mirage/utils/read-modules';
 import ENV from '../config/environment';
 import baseConfig, { testConfig } from '../mirage/config';
 import Server from 'ember-cli-mirage/server';
+import _assign from 'lodash/object/assign';
 
 export default {
   name: 'ember-cli-mirage',
@@ -14,7 +15,7 @@ export default {
 
     if (_shouldUseMirage(environment, ENV['ember-cli-mirage'])) {
       let modules = readModules(ENV.modulePrefix);
-      let options = _.assign(modules, {environment, baseConfig, testConfig})
+      let options = _assign(modules, {environment, baseConfig, testConfig})
 
       new Server(options);
     }