40584240957c5a226e514f88c7815981251c6005
Edward Faulkner <ef@alum.mit.edu>
2015-11-03 04:05:48 -0500

    Don't mutate model classes
    Model classes with associations can only be correctly registered with a
    Schema once, because the registration process mutates them. This change
    extends them instead so that we don't mutate the originals.
    
    I needed to switch to `for ... in` instead of `Object.keys` because now
    we definitely need to walk the prototype chain to find
    associations. This was probably a separate latent bug, since users can
    put associations onto ancestor classes.

---
+++ tests/integration/schema/reinitialize-associations-test.js
@@ -0,0 +1,38 @@
+import Mirage from 'ember-cli-mirage';
+import Model from 'ember-cli-mirage/orm/model';
+import Schema from 'ember-cli-mirage/orm/schema';
+import Db from 'ember-cli-mirage/db';
+import {module, test} from 'qunit';
+
+// Model classes are defined statically, just like in a typical app
+var User = Model.extend({
+  address: Mirage.hasMany()
+});
+var Address = Model.extend();
+
+module('Integration | Schema | reinitialize associations', {
+  beforeEach: function() {
+    this.db = new Db({
+      users: [],
+      addresses: []
+    });
+    this.schema = new Schema(this.db);
+
+    this.schema.registerModels({
+      address: Address,
+      user: User
+    });
+
+    this.schema.user.create({ id: 1, name: 'Link' });
+    this.schema.address.create({ id: 1, country: 'Hyrule', userId: 1 });
+  }
+});
+
+// By running two tests, we force the statically-defined classes to be
+// registered twice.
+test('safely initializes associations', function(assert) {
+  assert.equal(this.schema.user.find(1).address[0].country, 'Hyrule');
+});
+test('safely initializes associations again', function(assert) {
+  assert.equal(this.schema.user.find(1).address[0].country, 'Hyrule');
+});
