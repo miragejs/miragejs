f4a20066900d8b5b0f3dbc97974fd993448cf00f
Sam Selikoff <sam.selikoff@gmail.com>
2016-01-24 11:34:05 -0500

    Adds failing test for custom normalize function (#506)

---
+++ tests/integration/server/customized-normalize-method-test.js
@@ -0,0 +1,69 @@
+import {module, test} from 'qunit';
+import { Model, ActiveModelSerializer } from 'ember-cli-mirage';
+import { camelize } from 'ember-cli-mirage/utils/inflector';
+import Server from 'ember-cli-mirage/server';
+
+module('Integration | Server | Customized normalize method', {
+  beforeEach: function() {
+    this.server = new Server({
+      environment: 'test',
+      models: {
+        contact: Model
+      },
+      serializers: {
+        application: ActiveModelSerializer,
+        contact: ActiveModelSerializer.extend({
+          normalize(payload) {
+            let attrs = payload.some.random[1].attrs;
+            Object.keys(attrs).forEach(camelize);
+
+            let jsonApiDoc = {
+              data: {
+                type: 'contacts',
+                attributes: attrs
+              }
+            };
+            return jsonApiDoc;
+          }
+        })
+      }
+    });
+    this.server.timing = 0;
+    this.server.logging = false;
+  },
+  afterEach: function() {
+    this.server.shutdown();
+  }
+});
+
+test('custom model-specific normalize functions are used', function(assert) {
+  let server = this.server;
+  assert.expect(3);
+  let done = assert.async();
+
+  server.post('/contacts');
+
+  $.ajax({
+    method: 'POST',
+    url: '/contacts',
+    data: JSON.stringify({
+      some: {
+        random: [
+          {
+            format: true
+          },
+          {
+            attrs: {
+              first_name: 'Zelda'
+            }
+          }
+        ]
+      }
+    })
+  }).done((res, status, xhr) => {
+    assert.equal(xhr.status, 201);
+    assert.equal(server.db.contacts.length, 1);
+    assert.equal(server.db.contacts[0].firstName, 'Zelda');
+    done();
+  });
+});
