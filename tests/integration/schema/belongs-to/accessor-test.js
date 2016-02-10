9fd88d81a251961231ec7245a155257f9d6aa963
Edward Faulkner <ef@alum.mit.edu>
2016-02-09 23:25:57 -0500

    Fix belongsTo relationship when foreign key is 0

--- tests/integration/schema/belongs-to/accessor-test.js
+++ tests/integration/schema/belongs-to/accessor-test.js
@@ -1,4 +1,8 @@
 import BelongsToHelper from './belongs-to-helper';
+import Mirage from 'ember-cli-mirage';
+import Model from 'ember-cli-mirage/orm/model';
+import Schema from 'ember-cli-mirage/orm/schema';
+import Db from 'ember-cli-mirage/db';
 import {module, test} from 'qunit';
 
 module('Integration | Schema | belongsTo #accessor', {
@@ -27,4 +31,23 @@ module('Integration | Schema | belongsTo #accessor', {
     assert.equal(address.userId, user ? user.id : null, 'the modelId reference is correct');
   });
 
+});
+
+test('belongsTo accessors works when foreign key is present but falsy', function(assert) {
+  let db = new Db({
+    users: [],
+    addresses: []
+  });
+
+  let schema = new Schema(db, {
+    user: Model.extend(),
+    address: Model.extend({
+      user: Mirage.belongsTo()
+    })
+  });
+
+  db.users.insert({id: 0, name: 'some user' });
+  let insertedAddress = db.addresses.insert({ name: 'foo', userId: 0 });
+  let relatedUser = schema.address.find(insertedAddress.id).user;
+  assert.equal('some user', relatedUser ? relatedUser.name : undefined);
 });
