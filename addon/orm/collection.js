7a02e640a4583725dc906fc6ee3f6335ba697863
Liz Baillie <liz@lizbaillie.com>
2016-05-10 11:14:44 -0700

    adds YUIDoc comments to orm/collection.js (#731)

--- addon/orm/collection.js
+++ addon/orm/collection.js
@@ -1,10 +1,13 @@
 import _invoke from 'lodash/collection/invoke';
 import assert from '../assert';
 
-/*
-  An array of models, returned from one of the schema query
-  methods (all, find, where). Knows how to update and destroy its models.
-*/
+/**
+ * An array of models, returned from one of the schema query
+ * methods (all, find, where). Knows how to update and destroy its models.
+ * @class Collection
+ * @constructor
+ * @public
+ */
 export default class Collection {
   constructor(modelName, models = []) {
     assert(
@@ -16,36 +19,74 @@ export default class Collection {
     this.models = models;
   }
 
+  /**
+   * Updates each model in the collection (persisting immediately to the db).
+   * @method update
+   * @param key
+   * @param val
+   * @return this
+   * @public
+   */
   update(...args) {
     _invoke(this.models, 'update', ...args);
 
     return this;
   }
 
+  /**
+   * Destroys the db record for all models in the collection.
+   * @method destroy
+   * @return this
+   * @public
+   */
   destroy() {
     _invoke(this.models, 'destroy');
 
     return this;
   }
 
+  /**
+   * Saves all models in the collection.
+   * @method save
+   * @return this
+   * @public
+   */
   save() {
     _invoke(this.models, 'save');
 
     return this;
   }
 
+  /**
+   * Reloads each model in the collection.
+   * @method reload
+   * @return this
+   * @public
+   */
   reload() {
     _invoke(this.models, 'reload');
 
     return this;
   }
 
+  /**
+   * @method filter
+   * @param f
+   * @return {Collection}
+   * @public
+   */
   filter(f) {
     let filteredModels = this.models.filter(f);
 
     return new Collection(this.modelName, filteredModels);
   }
 
+  /**
+   * @method mergeCollection
+   * @param collection
+   * @return this
+   * @public
+   */
   mergeCollection(collection) {
     this.models = this.models.concat(collection.models);
 
