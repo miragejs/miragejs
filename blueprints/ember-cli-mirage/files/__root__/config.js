039fa3b1b9ad5eedb199886873d84c06719904c7
Andrew Lechowicz <andrew@alecho.com>
2016-12-09 10:16:57 -0500

    Clarify need for slashes in namespace (#963)

--- blueprints/ember-cli-mirage/files/__root__/config.js
+++ blueprints/ember-cli-mirage/files/__root__/config.js
@@ -9,7 +9,7 @@ export default function() {
   */
 
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
+  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
   /*
