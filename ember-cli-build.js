5da75536462259a641711e3018aa48f065db5a7b
Eric Kelly <heroiceric@gmail.com>
2017-07-07 06:26:50 -0700

    Replace "babel" -> "ember-cli-babel" (#1124)
    This satisfies the following deprecation warning:
    
    ```
    DEPRECATION: Putting the "includePolyfill" option in "babel" is deprecated, please put it in "ember-cli-babel" instead.
    ```

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    babel: {
+    'ember-cli-babel': {
       includePolyfill: true
     },
   });
