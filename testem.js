354a9a62d77034a8586b16afb0f364ca1df772b8
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-07-11 14:14:30 +0100

    Use headless Chrome of TravisCI (#1128)
    * CI: Use "chrome" addon on containerized runners
    
    * testem: Use headless Chrome by default
    
    * CI: Remove phantomjs

--- testem.js
+++ testem.js
@@ -8,5 +8,13 @@ module.exports = {
   ],
   "launch_in_dev": [
     "Chrome"
-  ]
+  ],
+  browser_args: {
+    Chrome: [
+      '--disable-gpu',
+      '--headless',
+      '--remote-debugging-port=9222',
+      '--window-size=1440,900'
+    ]
+  }
 };
