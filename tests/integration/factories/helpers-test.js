079c47711c64fca3c07bf7089ab466479f88097f
Vincent Eberle <veberle162@gmail.com>
2017-08-31 13:11:59 -0400

    #1076 Add breaking test for using 'association' helper in a dasherized factory

--- tests/integration/factories/helpers-test.js
+++ tests/integration/factories/helpers-test.js
@@ -3,46 +3,82 @@ import { Model, Factory, belongsTo, hasMany, trait, association } from 'ember-cl
 import Server from 'ember-cli-mirage/server';
 
 module('Integration | Server | Factories | helpers', {
-  beforeEach() {
-    this.server = new Server({
-      environment: 'test',
-      models: {
-        author: Model.extend({
-          posts: hasMany()
-        }),
-        category: Model.extend({
-          posts: hasMany('post', { inverse: 'kind' })
-        }),
-        post: Model.extend({
-          author: belongsTo(),
-          kind: belongsTo('category')
-        })
-      },
-      factories: {
-        author: Factory.extend({
-          name: 'Sam'
-        }),
-        category: Factory.extend({
-          name: 'awesome software'
-        }),
-        post: Factory.extend({
-          title: 'Lorem ipsum',
-
-          author: association(),
-
-          withCategory: trait({
-            kind: association()
-          })
-        })
-      }
-    });
-  },
   afterEach() {
     this.server.shutdown();
   }
 });
 
+test('it creates associations with "association" helper in a dasherized factory', function(assert) {
+  this.server = new Server({
+    environment: 'test',
+    models: {
+      author: Model.extend({
+        blogPosts: hasMany()
+      }),
+      blogPost: Model.extend({
+        author: belongsTo()
+      })
+    },
+    factories: {
+      author: Factory.extend({
+        name: 'Sam'
+      }),
+      blogPost: Factory.extend({
+        title: 'Lorem ipsum',
+
+        author: association()
+      })
+    }
+  });
+
+  let blogPost = this.server.create('blog-post');
+
+  assert.ok(blogPost.author);
+
+  let { db } = this.server;
+
+  assert.equal(db.authors.length, 1);
+  assert.deepEqual(db.authors[0], {
+    id: '1',
+    name: 'Sam',
+    blogPostIds: ['1']
+  });
+});
+
 test('it creates associations with "association" helper combininig with traits', function(assert) {
+  this.server = new Server({
+    environment: 'test',
+    models: {
+      author: Model.extend({
+        posts: hasMany()
+      }),
+      category: Model.extend({
+        posts: hasMany('post', { inverse: 'kind' })
+      }),
+      post: Model.extend({
+        author: belongsTo(),
+        kind: belongsTo('category')
+      })
+    },
+    factories: {
+      author: Factory.extend({
+        name: 'Sam'
+      }),
+      category: Factory.extend({
+        name: 'awesome software'
+      }),
+      post: Factory.extend({
+        title: 'Lorem ipsum',
+
+        author: association(),
+
+        withCategory: trait({
+          kind: association()
+        })
+      })
+    }
+  });
+
   let post = this.server.create('post', 'withCategory');
 
   assert.ok(post.kind);
