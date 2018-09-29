f91858a5cd5fb5723eeecdaa0f08512726a69e40
Sam Selikoff <sam.selikoff@gmail.com>
2018-09-28 23:02:25 -0400

    Tweaks

--- config/addon-docs.js
+++ config/addon-docs.js
@@ -6,4 +6,7 @@ const AddonDocsConfig = require('ember-cli-addon-docs/lib/config');
 module.exports = class extends AddonDocsConfig {
   // See https://ember-learn.github.io/ember-cli-addon-docs/docs/deploying
   // for details on configuration you can override here.
+
+  brand: 'blue'
+
 };
