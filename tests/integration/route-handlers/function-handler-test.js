8b297974aa4af6b313a374716c623cc549888eec
Sam Selikoff <sam.selikoff@gmail.com>
2018-11-14 15:58:48 -0500

    Cleaning up old acceptance tests

--- tests/integration/route-handlers/function-handler-test.js
+++ tests/integration/route-handlers/function-handler-test.js
@@ -1,5 +1,7 @@
 import { module, test } from 'qunit';
 import { Promise } from 'rsvp';
+import $ from 'jquery';
+const { ajax } = $;
 
 import { Model, Collection, Serializer } from 'ember-cli-mirage';
 import Server from 'ember-cli-mirage/server';
@@ -105,11 +107,67 @@ module('Integration | Route handlers | Function handler', function(hooks) {
     assert.equal(data, '');
   });
 
-  test('#serialize uses the default serializer on a model', function(assert) {
+  test('function can customize the reponse code via the 3rd argument', async function(assert) {
+    assert.expect(1);
     this.schema.users.create({ name: 'Sam' });
 
-    let user = this.schema.users.first();
-    let json = this.functionHandler.serialize(user);
+    this.server.get('/users/1', schema => {
+      return schema.users.first();
+    }, 304);
+
+    let request = await promiseAjax({ method: 'GET', url: '/users/1' });
+    assert.equal(request.xhr.status, 304);
+  });
+
+  test('function can return an instance of Response', async function(assert) {
+    let done = assert.async();
+    assert.expect(2);
+
+    this.server.get('/users', schema => {
+      return new Response(422, { some: 'header' }, { some: 'data' });
+    });
+
+    try {
+      await ajax({ url: '/users' });
+    } catch(request) {
+      assert.equal(request.status, 422);
+      assert.deepEqual(request.responseJSON, { some: 'data' });
+    } finally {
+      done();
+    }
+  });
+
+  test('returning a non-blank response from a custom handler whose default status is 204 changes the status to 200', async function(assert) {
+    let done = assert.async();
+    assert.expect(2);
+
+    this.server.put('/users', () => {
+      return { some: 'data' };
+    });
+
+    try {
+      let request = await promiseAjax({
+        url: `/users`,
+        method: 'PUT',
+        data: JSON.stringify({ request: 'data' })
+      });
+
+      assert.equal(request.xhr.status, 200, 'The status code is 200 instead of 204');
+      assert.deepEqual(request.data, { some: 'data' }, 'The response is correct');
+    } finally {
+      done();
+    }
+  });
+
+  test('#serialize uses the default serializer on a model', async function(assert) {
+    let done = assert.async();
+    this.schema.users.create({ name: 'Sam' });
+
+    this.server.get('/some-request', function(schema) {
+      return this.serialize(schema.users.first());
+    });
+
+    let json = await ajax({ url: '/some-request' });
 
     assert.deepEqual(json, {
       user: {
@@ -117,27 +175,37 @@ module('Integration | Route handlers | Function handler', function(hooks) {
         name: 'Sam'
       }
     });
+    done();
   });
 
-  test('#serialize uses the default serializer on a collection', function(assert) {
+  test('#serialize uses the default serializer on a collection', async function(assert) {
+    let done = assert.async();
     this.schema.users.create({ name: 'Sam' });
 
-    let users = this.schema.users.all();
-    let json = this.functionHandler.serialize(users);
+    this.server.get('/some-request', function(schema) {
+      return this.serialize(schema.users.all());
+    });
+
+    let json = await ajax({ url: '/some-request' });
 
     assert.deepEqual(json, {
       users: [
         { id: '1', name: 'Sam' }
       ]
     });
+    done();
   });
 
-  test('#serialize takes an optional serializer type', function(assert) {
+  test('#serialize takes an optional serializer type', async function(assert) {
+    let done = assert.async();
     this.schema.users.create({ name: 'Sam', tall: true, evil: false });
     this.schema.users.create({ name: 'Ganondorf', tall: true, evil: true });
 
-    let users = this.schema.users.all();
-    let json = this.functionHandler.serialize(users, 'sparse-user');
+    this.server.get('/some-request', function(schema) {
+      return this.serialize(schema.users.all(), 'sparse-user');
+    });
+
+    let json = await ajax({ url: '/some-request' });
 
     assert.deepEqual(json, {
       users: [
@@ -145,6 +213,7 @@ module('Integration | Route handlers | Function handler', function(hooks) {
         { id: '2', name: 'Ganondorf', tall: true }
       ]
     });
+    done();
   });
 
   test('#serialize throws an error when trying to specify a serializer that doesnt exist', function(assert) {
