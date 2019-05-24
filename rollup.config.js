3f1b06a4d0f4f92d738bc34ee16a0754d7b3e745
Sam Selikoff <sam.selikoff@gmail.com>
2019-05-24 15:51:32 -0400

    Setup Rollup

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+/* global process */
+import path from 'path';
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import alias from 'rollup-plugin-alias';
+
+export default {
+  input: 'lib/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'umd',
+    name: 'MirageJS.Server'
+  },
+  plugins: [
+    commonjs(),
+    alias({
+      '@miragejs/server': path.resolve(process.cwd(), './')
+    }),
+    resolve(),
+    babel({
+      babelrc: false,
+      comments: false,
+      presets: [
+        ['@babel/preset-env', {
+          modules: false
+        }]
+      ]
+    })
+  ]
+};
