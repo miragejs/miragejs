ed55af564422c578028ca71234a4ec81dce9337d
Alexandre Santos <alexandre.santozz@gmail.com>
2019-07-24 16:19:37 +0100

    Run codemods route-handlers

--- __tests__/unit/route-handlers/shorthands/base-test.js
+++ __tests__/unit/route-handlers/shorthands/base-test.js
@@ -1,62 +1,62 @@
-import { _routeHandlersShorthandsBase as BaseShorthandRouteHandler } from '@miragejs/server';
+import { _routeHandlersShorthandsBase as BaseShorthandRouteHandler } from '../../../../lib';
 
-import {module, test} from 'qunit';
-
-module('Unit | Route handlers | Shorthands | BaseShorthandRouteHandler', function(hooks) {
-  hooks.beforeEach(function() {
-    this.handler = new BaseShorthandRouteHandler();
-    this.request = { params: { id: '' } };
+describe('Unit | Route handlers | Shorthands | BaseShorthandRouteHandler', function() {
+  let handler = null;
+  let request = null;
+  beforeEach(function() {
+    handler = new BaseShorthandRouteHandler();
+    request = { params: { id: '' } };
   });
 
-  test('it returns a number if it\'s a number', function(assert) {
-    this.request.params.id = 2;
-    assert.equal(this.handler._getIdForRequest(this.request), 2, 'it returns a number');
+  test('it returns a number if it\'s a number', () => {
+    request.params.id = 2;
+    expect(handler._getIdForRequest(request)).toEqual(2);
   });
 
-  test('it returns a number if it\'s a string represented number', function(assert) {
-    this.request.params.id = '2';
-    assert.equal(this.handler._getIdForRequest(this.request), 2, 'it returns a number');
+  test('it returns a number if it\'s a string represented number', () => {
+    request.params.id = '2';
+    expect(handler._getIdForRequest(request)).toEqual('2');
   });
 
-  test('it returns a string it\'s a dasherized number', function(assert) {
-    this.request.params.id = '2-1';
-    assert.equal(this.handler._getIdForRequest(this.request), '2-1', 'it returns a number');
+  test('it returns a string it\'s a dasherized number', () => {
+    request.params.id = '2-1';
+    expect(handler._getIdForRequest(request)).toEqual('2-1');
   });
 
-  test('it returns a string if it\'s a string', function(assert) {
-    this.request.params.id = 'someID';
-    assert.equal(this.handler._getIdForRequest(this.request), 'someID', 'it returns a number');
+  test('it returns a string if it\'s a string', () => {
+    request.params.id = 'someID';
+    expect(handler._getIdForRequest(request)).toEqual('someID');
   });
 
-  test('getModelClassFromPath works with various named route path variable', function(assert) {
+  test('getModelClassFromPath works with various named route path variable', () => {
     let urlWithSlash = '/api/fancy-users';
     let urlWithIdAndSlash = '/api/fancy-users/:id';
 
-    assert.equal(this.handler.getModelClassFromPath(urlWithSlash), 'fancy-user', 'it returns a singular model name');
-    assert.equal(this.handler.getModelClassFromPath(urlWithIdAndSlash, true), 'fancy-user', 'it returns a singular model name');
+    expect(handler.getModelClassFromPath(urlWithSlash)).toEqual('fancy-user');
+    expect(handler.getModelClassFromPath(urlWithIdAndSlash, true)).toEqual('fancy-user');
 
     urlWithSlash = '/api/exquisite-users';
     urlWithIdAndSlash = '/api/exquisite-users/:objectId';
 
-    assert.equal(this.handler.getModelClassFromPath(urlWithSlash), 'exquisite-user', 'it returns a singular model name');
-    assert.equal(this.handler.getModelClassFromPath(urlWithIdAndSlash, true), 'exquisite-user', 'it returns a singular model name');
+    expect(handler.getModelClassFromPath(urlWithSlash)).toEqual('exquisite-user');
+    expect(handler.getModelClassFromPath(urlWithIdAndSlash, true)).toEqual('exquisite-user');
 
     urlWithSlash = '/api/elegant-users';
     urlWithIdAndSlash = '/api/elegant-users/:firstName/:lastName';
 
-    assert.equal(this.handler.getModelClassFromPath(urlWithSlash), 'elegant-user', 'it returns a singular model name');
-    assert.equal(this.handler.getModelClassFromPath(urlWithIdAndSlash, true), 'elegant-user', 'it returns a singular model name');
+    expect(handler.getModelClassFromPath(urlWithSlash)).toEqual('elegant-user');
+    expect(handler.getModelClassFromPath(urlWithIdAndSlash, true)).toEqual('elegant-user');
   });
 
-  test('it can read the id from the url', function(assert) {
+  test('it can read the id from the url', () => {
     let request = { params: { id: 'test-id' } };
-    assert.equal(this.handler._getIdForRequest(request), 'test-id', 'it returns id from url parameters.');
+    expect(handler._getIdForRequest(request)).toEqual('test-id');
   });
 
-  test('it can read the id from the request body', function(assert) {
+  test('it can read the id from the request body', () => {
     let request = { params: {} };
     let jsonApiDoc = { data: { id: 'jsonapi-id' } };
-    assert.equal(this.handler._getIdForRequest(request, jsonApiDoc), 'jsonapi-id', 'it returns id from json api data.');
+    expect(handler._getIdForRequest(request, jsonApiDoc)).toEqual('jsonapi-id');
   });
 
 });
