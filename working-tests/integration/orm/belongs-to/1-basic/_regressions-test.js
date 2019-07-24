b5b224687d6aa4c8b732b00196e0a4de5790e90b
Alexandre Santos <alexandre.santozz@gmail.com>
2019-07-24 11:36:59 +0100

    Migrate integration/orm 1 and 2

---
+++ working-tests/integration/orm/belongs-to/1-basic/_regressions-test.js
@@ -0,0 +1,25 @@
+import { Model, belongsTo, _Schema as Schema, _Db as Db } from '@miragejs/server';
+import { module, test } from 'qunit';
+
+module('Integration | ORM | Belongs To | Basic | regressions', function() {
+  test('belongsTo accessors works when foreign key is present but falsy', function(assert) {
+    let db = new Db({
+      posts: [
+        { id: 1, authorId: 0, name: 'some post' }
+      ],
+      authors: [
+        { id: 0, name: 'Foo' }
+      ]
+    });
+
+    let schema = new Schema(db, {
+      author: Model.extend(),
+      post: Model.extend({
+        author: belongsTo()
+      })
+    });
+
+    let post = schema.posts.find(1);
+    assert.equal(post.author.name, 'Foo');
+  });
+});
