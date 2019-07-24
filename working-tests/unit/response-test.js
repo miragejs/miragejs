e21c1de7a849cf9c708cc42b41dc76dc3221de53
Alexandre Santos <alexandre.santozz@gmail.com>
2019-07-24 11:23:03 +0100

    Add unit tests from root folder

---
+++ working-tests/unit/response-test.js
@@ -0,0 +1,33 @@
+import { Response } from '@miragejs/server';
+
+import { module, test } from 'qunit';
+
+module('Unit | Response', function() {
+  test('it can be instantiated and return a rack response', function(assert) {
+    let response = new Response(404, {}, {});
+
+    assert.ok(response);
+    assert.ok(response.toRackResponse());
+  });
+
+  test('it can be instantiated with just a response code', function(assert) {
+    let response = new Response(404);
+
+    assert.ok(response);
+    assert.ok(response.toRackResponse());
+  });
+
+  test('it adds Content-Type by default', function(assert) {
+    let response = new Response(200, {}, {});
+
+    assert.ok(response);
+    assert.equal(response.headers['Content-Type'], 'application/json');
+  });
+
+  test('it does not add Content-Type for a 204 response', function(assert) {
+    let response = new Response(204);
+
+    assert.ok(response);
+    assert.notOk(response.headers['Content-Type']);
+  });
+});
