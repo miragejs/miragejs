713648dee5f97a9e3104151d3aa6f40a875eaff0
Sam Selikoff <sam.selikoff@gmail.com>
2019-07-27 13:39:19 -0400

    normalize-request-attrs-test works

--- __tests__/integration/route-handlers/function-handler/normalize-request-attrs-test.js
+++ __tests__/integration/route-handlers/function-handler/normalize-request-attrs-test.js
@@ -1,178 +1,165 @@
+import { Server, Model, ActiveModelSerializer } from "@miragejs/server";
 
-import { Model, ActiveModelSerializer } from "@miragejs/server";
-import Server from "@miragejs/server/server";
-import promiseAjax from "../../../helpers/promise-ajax";
+describe("Integration | Route handlers | Function handler | #normalizedRequestAttrs", () => {
+  let server;
+  beforeEach(() => {
+    server = new Server({
+      environment: "development",
+      models: {
+        user: Model.extend({}),
+        fineComment: Model.extend({})
+      },
+      serializers: {
+        application: ActiveModelSerializer
+      }
+    });
+    server.timing = 0;
+    server.logging = false;
+  });
 
-describe(
-  "Integration | Route handlers | Function handler | #normalizedRequestAttrs",
-  () => {
-    let server; beforeEach(() => {
-      server = new Server({
-        environment: "development",
-        models: {
-          user: Model.extend({}),
-          fineComment: Model.extend({})
-        },
-        serializers: {
-          application: ActiveModelSerializer
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  test(`it returns an object with the primary resource's attrs and belongsTo keys camelized`, async () => {
+    expect.assertions(1);
+
+    server.post("/users", function() {
+      let attrs = this.normalizedRequestAttrs();
+
+      expect(attrs).toEqual({
+        firstName: "Sam",
+        lastName: "Selikoff",
+        teamId: 1
+      });
+
+      return {};
+    });
+
+    await fetch("/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user: {
+          first_name: "Sam",
+          last_name: "Selikoff",
+          team_id: 1
         }
+      })
+    });
+  });
+
+  test(`it works for compound names`, async () => {
+    expect.assertions(1);
+
+    server.post("/fine-comments", function() {
+      let attrs = this.normalizedRequestAttrs();
+
+      expect(attrs).toEqual({
+        shortText: "lorem ipsum"
       });
-      server.timing = 0;
-      server.logging = false;
+
+      return {};
     });
 
-    afterEach(() => {
-      server.shutdown();
-    });
-
-    test(`it returns an object with the primary resource's attrs and belongsTo keys camelized`, async () => {
-      assert.expect(1);
-
-      server.post("/users", function() {
-        let attrs = normalizedRequestAttrs();
-
-        expect(attrs).toEqual({
-          firstName: "Sam",
-          lastName: "Selikoff",
-          teamId: 1
-        });
-
-        return {};
-      });
-
-      await promiseAjax({
-        method: "POST",
-        url: "/users",
-        contentType: "application/json",
-        data: JSON.stringify({
-          user: {
-            first_name: "Sam",
-            last_name: "Selikoff",
-            team_id: 1
-          }
-        })
-      });
-    });
-
-    test(`it works for compound names`, async () => {
-      assert.expect(1);
-
-      server.post("/fine-comments", function() {
-        let attrs = normalizedRequestAttrs();
-
-        expect(attrs).toEqual({
-          shortText: "lorem ipsum"
-        });
-
-        return {};
-      });
-
-      await promiseAjax({
-        method: "POST",
-        url: "/fine-comments",
-        contentType: "application/json",
-        data: JSON.stringify({
-          fine_comment: {
-            short_text: "lorem ipsum"
-          }
-        })
-      });
-    });
-
-    test(`it shows a meaningful error message if it cannot infer the modelname from the URL`, async () => {
-      assert.expect(1);
-
-      server.post("/users/create", function() {
-        normalizedRequestAttrs();
-      });
-
-      assert.rejects(
-        promiseAjax({ method: "POST", url: "/users/create" }),
-        function(ajaxError) {
-          return (
-            ajaxError.xhr.responseText.indexOf(
-              `the detected model of 'create' does not exist`
-            ) > -1
-          );
+    await fetch("/fine-comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        fine_comment: {
+          short_text: "lorem ipsum"
         }
-      );
+      })
+    });
+  });
+
+  test(`it shows a meaningful error message if it cannot infer the modelname from the URL`, async () => {
+    expect.assertions(2);
+
+    server.post("/users/create", function() {
+      this.normalizedRequestAttrs();
     });
 
-    test(`it accepts an optional modelName if it cannot be inferred from the path `, async () => {
-      assert.expect(1);
+    let res = await fetch("/users/create", { method: "POST" });
+    let data = await res.json();
 
-      server.post("/users/create", function() {
-        let attrs = normalizedRequestAttrs("user");
+    expect(res.status).toEqual(500);
+    expect(data.message).toMatch(
+      "the detected model of 'create' does not exist"
+    );
+  });
 
-        expect(attrs).toEqual({
-          firstName: "Sam",
-          lastName: "Selikoff",
-          teamId: 1
-        });
+  test(`it accepts an optional modelName if it cannot be inferred from the path `, async () => {
+    expect.assertions(1);
 
-        return {};
+    server.post("/users/create", function() {
+      let attrs = this.normalizedRequestAttrs("user");
+
+      expect(attrs).toEqual({
+        firstName: "Sam",
+        lastName: "Selikoff",
+        teamId: 1
       });
 
-      await promiseAjax({
-        method: "POST",
-        url: "/users/create",
-        contentType: "application/json",
-        data: JSON.stringify({
-          user: {
-            first_name: "Sam",
-            last_name: "Selikoff",
-            team_id: 1
-          }
-        })
-      });
+      return {};
     });
 
-    test(`it errors if the optional parameter is camelized for a model with a compount name`, async () => {
-      assert.expect(1);
-
-      server.post("/fine-comments/create", function() {
-        normalizedRequestAttrs("fineComment");
-      });
-
-      assert.rejects(
-        promiseAjax({ method: "POST", url: "/fine-comments/create" }),
-        function(ajaxError) {
-          return (
-            ajaxError.xhr.responseText.indexOf(
-              `You called normalizedRequestAttrs('fineComment'), but normalizedRequestAttrs was intended to be used with the dasherized version of the model type. Please change this to normalizedRequestAttrs('fine-comment')`
-            ) > 0
-          );
+    await fetch("/users/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user: {
+          first_name: "Sam",
+          last_name: "Selikoff",
+          team_id: 1
         }
-      );
+      })
+    });
+  });
+
+  test(`it errors if the optional parameter is camelized for a model with a compount name`, async () => {
+    expect.assertions(2);
+
+    server.post("/fine-comments/create", function() {
+      this.normalizedRequestAttrs("fineComment");
     });
 
-    test(`it works with a form encoded request that has a lower-case content-type (issue 1398)`, async () => {
-      assert.expect(1);
+    let res = await fetch("/fine-comments/create", { method: "POST" });
+    let data = await res.json();
 
-      server.post("/form-test", function() {
-        // Easiest way I could figure out to change the capitalization of the Content-Type header. Tried
-        // to do this from the Ajax side but jquery kept capitalizing the header.
-        request.requestHeaders[
-          "content-type"
-        ] = request.requestHeaders["Content-Type"];
-        delete request.requestHeaders["Content-Type"];
+    expect(res.status).toEqual(500);
+    expect(data.message).toMatch(
+      "You called normalizedRequestAttrs('fineComment'), but normalizedRequestAttrs was intended to be used with the dasherized version of the model type. Please change this to normalizedRequestAttrs('fine-comment')"
+    );
+  });
 
-        let attrs = normalizedRequestAttrs("user");
+  test(`it works with a form encoded request that has a lower-case content-type (issue 1398)`, async () => {
+    expect.assertions(1);
 
-        expect(attrs).toEqual({
-          name: "Sam Selikoff",
-          company: "TED",
-          email: "sam.selikoff@gmail.com"
-        });
+    server.post("/form-test", function() {
+      let attrs = this.normalizedRequestAttrs("user");
 
-        return {};
+      expect(attrs).toEqual({
+        name: "Sam Selikoff",
+        company: "TED",
+        email: "sam.selikoff@gmail.com"
       });
 
-      await promiseAjax({
-        method: "POST",
-        url: "/form-test",
-        data: "name=Sam+Selikoff&company=TED&email=sam.selikoff@gmail.com"
-      });
+      return {};
     });
-  }
-);
+
+    await fetch("/form-test", {
+      method: "POST",
+      headers: {
+        "content-type": "x-www-form-urlencoded"
+      },
+      body: "name=Sam+Selikoff&company=TED&email=sam.selikoff@gmail.com"
+    });
+  });
+});
