501838211fb37dc275561ff5403fcf7132db6883
Sam Selikoff <sam.selikoff@gmail.com>
2019-07-29 11:16:11 -0400

    rest-serializer-sanity-test works

--- __tests__/integration/server/shorthands/rest-serializer-sanity-test.js
+++ __tests__/integration/server/shorthands/rest-serializer-sanity-test.js
@@ -1,147 +1,139 @@
+import {
+  Server,
+  Model,
+  hasMany,
+  belongsTo,
+  RestSerializer
+} from "@miragejs/server";
 
-import { Model, hasMany, belongsTo, RestSerializer } from "@miragejs/server";
-import Server from "@lib/server";
-import promiseAjax from "../../../helpers/promise-ajax";
+describe("Integration | Server | Shorthands | REST Serializer Sanity check", function() {
+  let server;
 
-describe(
-  "Integration | Server | Shorthands | REST Serializer Sanity check",
-  function() {
-    beforeEach(function() {
-      this.server = new Server({
-        environment: "test",
-        models: {
-          contact: Model.extend({
-            addresses: hasMany()
-          }),
-          address: Model.extend({
-            contact: belongsTo()
-          })
-        },
-        serializers: {
-          application: RestSerializer.extend({
-            normalizeIds: true
-          })
+  beforeEach(function() {
+    server = new Server({
+      environment: "test",
+      models: {
+        contact: Model.extend({
+          addresses: hasMany()
+        }),
+        address: Model.extend({
+          contact: belongsTo()
+        })
+      },
+      serializers: {
+        application: RestSerializer.extend({
+          normalizeIds: true
+        })
+      }
+    });
+    server.timing = 0;
+    server.logging = false;
+  });
+
+  afterEach(function() {
+    server.shutdown();
+  });
+
+  test("a get shorthand works", async () => {
+    expect.assertions(2);
+
+    server.db.loadData({
+      contacts: [{ id: 1, name: "Link" }]
+    });
+
+    server.get("/contacts");
+
+    let res = await fetch("/contacts");
+    let data = await res.json();
+
+    expect(res.status).toEqual(200);
+    expect(data).toEqual({
+      contacts: [{ id: "1", name: "Link", addresses: [] }]
+    });
+  });
+
+  test("a post shorthand works", async () => {
+    expect.assertions(3);
+
+    server.db.loadData({
+      contacts: [{ id: 1, name: "Link" }]
+    });
+
+    server.post("/addresses");
+
+    let res = await fetch("/addresses", {
+      method: "POST",
+      body: JSON.stringify({
+        address: {
+          street: "5th ave",
+          contact: 1
         }
-      });
-      this.server.timing = 0;
-      this.server.logging = false;
+      })
     });
 
-    afterEach(function() {
-      this.server.shutdown();
+    expect(res.status).toEqual(201);
+    expect(server.db.addresses).toHaveLength(1);
+    expect(server.db.addresses[0].contactId).toEqual(1);
+  });
+
+  test("a put shorthand works", async () => {
+    expect.assertions(2);
+
+    server.db.loadData({
+      contacts: [{ id: 1, name: "Link" }]
     });
 
-    test("a get shorthand works", async () => {
-      expect.assertions(2);
+    server.put("/contacts/:id");
 
-      this.server.db.loadData({
-        contacts: [{ id: 1, name: "Link" }]
-      });
-
-      this.server.get("/contacts");
-
-      let { xhr, data } = await promiseAjax({
-        method: "GET",
-        url: "/contacts"
-      });
-
-      expect(xhr.status).toEqual(200);
-      expect(data).toEqual({
-        contacts: [{ id: "1", name: "Link", addresses: [] }]
-      });
+    let res = await fetch("/contacts/1", {
+      method: "PUT",
+      body: JSON.stringify({
+        contact: {
+          name: "Zelda"
+        }
+      })
     });
 
-    test("a post shorthand works", async () => {
-      let { server } = this;
-      expect.assertions(3);
+    expect(res.status).toEqual(200);
+    expect(server.db.contacts[0].name).toEqual("Zelda");
+  });
 
-      this.server.db.loadData({
-        contacts: [{ id: 1, name: "Link" }]
-      });
+  test("a patch shorthand works", async () => {
+    expect.assertions(2);
 
-      server.post("/addresses");
-
-      let { xhr } = await promiseAjax({
-        method: "POST",
-        url: "/addresses",
-        data: JSON.stringify({
-          address: {
-            street: "5th ave",
-            contact: 1
-          }
-        })
-      });
-
-      expect(xhr.status).toEqual(201);
-      expect(server.db.addresses.length).toEqual(1);
-      expect(server.db.addresses[0].contactId).toEqual(1);
+    server.db.loadData({
+      contacts: [{ id: 1, name: "Link" }]
     });
 
-    test("a put shorthand works", async () => {
-      let { server } = this;
-      expect.assertions(2);
+    server.patch("/contacts/:id");
 
-      this.server.db.loadData({
-        contacts: [{ id: 1, name: "Link" }]
-      });
-
-      server.put("/contacts/:id");
-
-      let { xhr } = await promiseAjax({
-        method: "PUT",
-        url: "/contacts/1",
-        data: JSON.stringify({
-          contact: {
-            name: "Zelda"
-          }
-        })
-      });
-
-      expect(xhr.status).toEqual(200);
-      expect(server.db.contacts[0].name).toEqual("Zelda");
+    let res = await fetch("/contacts/1", {
+      method: "PATCH",
+      body: JSON.stringify({
+        contact: {
+          name: "Zelda"
+        }
+      })
     });
 
-    test("a patch shorthand works", async () => {
-      let { server } = this;
-      expect.assertions(2);
+    expect(res.status).toEqual(200);
+    expect(server.db.contacts[0].name).toEqual("Zelda");
+  });
 
-      this.server.db.loadData({
-        contacts: [{ id: 1, name: "Link" }]
-      });
+  test("a delete shorthand works", async () => {
+    expect.assertions(2);
 
-      server.patch("/contacts/:id");
-
-      let { xhr } = await promiseAjax({
-        method: "PATCH",
-        url: "/contacts/1",
-        data: JSON.stringify({
-          contact: {
-            name: "Zelda"
-          }
-        })
-      });
-
-      expect(xhr.status).toEqual(200);
-      expect(server.db.contacts[0].name).toEqual("Zelda");
+    server.db.loadData({
+      contacts: [{ id: 1, name: "Link" }]
     });
 
-    test("a delete shorthand works", async () => {
-      let { server } = this;
-      expect.assertions(2);
+    server.del("/contacts/:id");
 
-      this.server.db.loadData({
-        contacts: [{ id: 1, name: "Link" }]
-      });
-
-      server.del("/contacts/:id");
-
-      let { xhr } = await promiseAjax({
-        method: "DELETE",
-        url: "/contacts/1"
-      });
-
-      expect(xhr.status).toEqual(204);
-      expect(server.db.contacts.length).toEqual(0);
+    let res = await fetch("/contacts/1", {
+      method: "DELETE"
     });
-  }
-);
+
+    expect(res.status).toEqual(204);
+    expect(server.db.contacts).toHaveLength(0);
+  });
+});
