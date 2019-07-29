a5aec3e9f3449dae2e32420c46d183605ec7a9a6
Sam Selikoff <sam.selikoff@gmail.com>
2019-07-29 16:15:29 -0400

    attempt to get jest-runner-eslint setup, fail

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+let browser = {
+  displayName: "browser",
   transformIgnorePatterns: ["<rootDir>/node_modules/(?!lodash-es)"],
   setupFilesAfterEnv: ["jest-extended"],
   testMatch: ["**/__tests__/**/*-test.[jt]s?(x)"],
@@ -6,4 +7,18 @@ module.exports = {
     "@lib(.*)": "<rootDir>/lib$1",
     "@miragejs/server": "<rootDir>/lib/index"
   }
+};
+
+// let eslint = {
+//   displayName: "lint",
+//   runner: "jest-runner-eslint",
+//   cliOptions: {
+//     config: "./.eslintrc.js"
+//   },
+//   // testMatch: ["<rootDir>/__tests__/**/*.js", "<rootDir>/lib/**/*.js"]
+//   testMatch: ["<rootDir>/lib/response.js"]
+// };
+
+module.exports = {
+  projects: [browser]
 };
