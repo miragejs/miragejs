f75ce3ae384b82bb2f48adabdca5700697bfbeee
Sam Selikoff <sam.selikoff@gmail.com>
2019-07-30 11:05:35 -0400

    use babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
   root: true,
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
+  parser: "babel-eslint",
   plugins: ["jest"],
   extends: [
     "eslint:recommended",
