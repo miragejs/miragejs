27e1ea53caa042d15d4955cfe2075a10f22ba705
Jonathan Haines <jonno.haines@gmail.com>
2019-09-11 15:42:16 +1000

    lint aliases, with prettier too

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   root: true,
   parser: "babel-eslint",
-  plugins: ["jest"],
+  plugins: ["import", "jest"],
   extends: [
     "eslint:recommended",
+    "plugin:import/errors",
     "plugin:jest/recommended",
-    "plugin:jest/style"
+    "plugin:jest/style",
+    "plugin:prettier/recommended"
   ],
   env: {
-    "jest/globals": true,
     es6: true,
     node: true,
     browser: true
@@ -21,11 +22,12 @@ module.exports = {
     "no-var": 0,
     "object-shorthand": 0,
     "arrow-parens": 0,
-    "no-unused-vars": ["error", { args: "none" }]
+    "no-unused-vars": ["error", { args: "none" }],
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": ["__tests__", "*.config.js"]}]
   },
   overrides: [
     {
-      files: ["jest.config.js", "babel.config.js"],
+      files: ["jest.config.js", "babel.config.js", "rollup.config.js", "rollup.test.config.js"],
       env: {
         browser: false,
         node: true
@@ -34,8 +36,19 @@ module.exports = {
     {
       files: ["__tests__/**"],
       env: {
-        browser: true
+        "jest/globals": true
       }
     }
-  ]
+  ],
+  settings: {
+    "import/resolver": {
+      alias: [
+        ["@lib", "./lib"],
+        ["@miragejs/server", "./index"]
+      ],
+      node: {
+        extensions: ["js"]
+      }
+    }
+  }
 };
