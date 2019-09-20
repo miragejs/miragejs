a00c6c2156df24802d01e0ae0dc6832d61710b66
Sam Selikoff <sam.selikoff@gmail.com>
2019-09-20 14:32:37 -0400

    Test other builds

--- jest.config.js
+++ jest.config.js
@@ -9,37 +9,33 @@ let internal = {
   }
 };
 
-// let esmBundle = {
-//   displayName: "esm bundle",
-//   transformIgnorePatterns: ["<rootDir>/node_modules/(?!lodash)"],
-//   setupFilesAfterEnv: ["jest-extended"],
-//   testMatch: ["**/__tests__/integration/**/*-test.[jt]s?(x)"],
-//   moduleNameMapper: {
-//     "@lib(.*)": "<rootDir>/dist/mirage-esm",
-//     "@miragejs/server": "<rootDir>/dist/mirage-esm"
-//   }
-// };
-//
-// let umdBundle = {
-//   displayName: "umd bundle",
-//   setupFilesAfterEnv: ["jest-extended"],
-//   testMatch: ["**/__tests__/integration/**/*-test.[jt]s?(x)"],
-//   moduleNameMapper: {
-//     "@lib(.*)": "<rootDir>/dist/mirage-umd",
-//     "@miragejs/server": "<rootDir>/dist/mirage-umd"
-//   }
-// };
-//
-// let cjsBundle = {
-//   displayName: "cjs bundle",
-//   transformIgnorePatterns: ["<rootDir>/node_modules/(?!lodash)"],
-//   setupFilesAfterEnv: ["jest-extended"],
-//   testMatch: ["**/__tests__/integration/**/*-test.[jt]s?(x)"],
-//   moduleNameMapper: {
-//     "@lib(.*)": "<rootDir>/dist/mirage-cjs",
-//     "@miragejs/server": "<rootDir>/dist/mirage-cjs"
-//   }
-// };
+// External API, Create React App-like environment.
+let browserEnvironmentConsumingEsm = {
+  displayName: "browserEnvironmentConsumingEsm",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["jest-extended"],
+  testMatch: [
+    "**/__tests__/external/shared/**/*-test.[jt]s?(x)",
+    "**/__tests__/external/browser-only/**/*-test.[jt]s?(x)"
+  ],
+  moduleNameMapper: {
+    "@miragejs/server": "<rootDir>/dist/mirage-esm.js"
+  }
+};
+
+// External API, Gatsby-like environment (SSR of client-side code)
+let nodeEnvironmentConsumingEsm = {
+  displayName: "nodeEnvironmentConsumingEsm",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["jest-extended"],
+  testMatch: [
+    "**/__tests__/external/shared/**/*-test.[jt]s?(x)",
+    "**/__tests__/external/node-only/**/*-test.[jt]s?(x)"
+  ],
+  moduleNameMapper: {
+    "@miragejs/server": "<rootDir>/dist/mirage-esm.js"
+  }
+};
 
 // External API, Create React App-like environment.
 let browserEnvironmentConsumingCjs = {
@@ -71,40 +67,27 @@ let nodeEnvironmentConsumingCjs = {
   }
 };
 
-// From Ryan's PR. Run all tests in Node. (Lots will fail.)
-// let node = {
-//   displayName: "node",
-//   transformIgnorePatterns: ["<rootDir>/node_modules/(?!lodash)"],
+// External API, script tag or Code Sandbox
+// let browserEnvironmentConsumingUmd = {
+//   displayName: "browserEnvironmentConsumingUmd",
+//   testEnvironment: "jsdom",
 //   setupFilesAfterEnv: ["jest-extended"],
-//   testEnvironment: "node",
-//   testMatch: ["**/__tests__/**/*-test.[jt]s?(x)"],
-//   testPathIgnorePatterns: [
-//     "<rootDir>/__tests__/browser-only/",
-//     "/node_modules/"
+//   testMatch: [
+//     "**/__tests__/external/shared/**/*-test.[jt]s?(x)",
+//     "**/__tests__/external/browser-only/**/*-test.[jt]s?(x)"
 //   ],
 //   moduleNameMapper: {
-//     "@lib(.*)": "<rootDir>/lib$1",
-//     "@miragejs/server": "<rootDir>/lib/index",
-//     pretender: "<rootDir>/shims/pretender-node.js"
+//     "@miragejs/server": "<rootDir>/dist/mirage-umd.js"
 //   }
 // };
 
-// let eslint = {
-//   displayName: "lint",
-//   runner: "jest-runner-eslint",
-//   cliOptions: {
-//     config: "./.eslintrc.js"
-//   },
-//   // testMatch: ["<rootDir>/__tests__/**/*.js", "<rootDir>/lib/**/*.js"]
-//   testMatch: ["<rootDir>/lib/response.js"]
-// };
-
 module.exports = {
-  // Only browser for now, but add back in bundles soon
-  // projects: [browser]
   projects: [
     internal,
+    browserEnvironmentConsumingEsm,
+    nodeEnvironmentConsumingEsm,
     browserEnvironmentConsumingCjs,
     nodeEnvironmentConsumingCjs
+    // browserEnvironmentConsumingUmd
   ]
 };
