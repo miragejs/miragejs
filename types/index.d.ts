e487aadeac6f6c5c3c4eb7cc3d045b877b3fab84
Zoltan <zdebre@gmail.com>
2020-02-02 12:24:49 +1300

    Change passthrough param to optional

--- types/index.d.ts
+++ types/index.d.ts
@@ -329,7 +329,7 @@ declare module "miragejs/server" {
     ): void;
 
     /** Pass through one or more URLs to make real requests. */
-    passthrough(urls: ((request: Request) => any) | string | string[]): void;
+    passthrough(urls?: ((request: Request) => any) | string | string[]): void;
 
     /** Load all available fixture data matching the given name(s). */
     loadFixtures(...names: string[]): void;
